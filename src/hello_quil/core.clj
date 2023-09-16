65c4b6248adca87e61314a99b3c78eb346b9f928
Martin Collberg <martin.collberg@gmail.com>
2023-09-16 19:49:50 +0200

    Refactor: Move 'circle' drawing and update into :circle-state

--- src/hello_quil/core.clj
+++ src/hello_quil/core.clj
@@ -1,48 +1,68 @@
 (ns hello-quil.core
   (:require [quil.core :as q]
-            [quil.middleware :as m]))
+            [quil.middleware :as m]
+            [clj-time [core :as t]]))
+
+(comment
+  (let [start-time (t/now)]
+    ... do lots of work ...
+    (t/in-millis (t/interval start-time (t/now))))
+  )
+
+
+
 
 (defn setup []
-  ; Set frame rate to 30 frames per second.
+                                        ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
-  ; Set color mode to HSB (HSV) instead of default RGB.
+                                        ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
-  ; setup function returns initial state. It contains
-  ; circle color and position.
-  {:color 0
-   :angle 0})
+                                        ; setup function returns initial state. It contains
+                                        ; circle color and position.
+  {:circle-anim {:color 0
+                 :angle 0
+                 :last-time (t/now) }})
+
+(defn update-circle
+  [state]
+  {:color (mod (+ (:color state) 0.7) 255)
+   :angle (+ (:angle state) 0.02)
+   :last-time (:last-time (t/now))})
 
 (defn update-state [state]
-  ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+  (let [now (t/now)
+        dt (t/in-millis (t/interval (:last-time state) now))]
+    (update-in state [:circle-anim] update-circle)))
 
-(defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
-  (q/background 240)
-  ; Set circle color.
+(defn draw-circle
+  [state]
   (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
+                                        ; Calculate x and y coordinates of the circle.
   (let [angle (:angle state)
         x (* 150 (q/cos angle))
         y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
+                                        ; Move origin point to the center of the sketch.
     (q/with-translation [(/ (q/width) 2)
                          (/ (q/height) 2)]
-      ; Draw the circle.
+                                        ; Draw the circle.
       (q/ellipse x y 100 100))))
 
 
+(defn draw-state [state]
+  (q/background 240)
+  (draw-circle (:circle-anim state)))
+
+
 (q/defsketch hello-quil
-  :title "You spin my circle right round"
+  :title "Quil ECS testing"
   :size [500 500]
-  ; setup function called only once, during sketch initialization.
+                                        ; setup function called only once, during sketch initialization.
   :setup setup
-  ; update-state is called on each iteration before draw-state.
+                                        ; update-state is called on each iteration before draw-state.
   :update update-state
   :draw draw-state
   :features [:keep-on-top]
-  ; This sketch uses functional-mode middleware.
-  ; Check quil wiki for more info about middlewares and particularly
-  ; fun-mode.
+                                        ; This sketch uses functional-mode middleware.
+                                        ; Check quil wiki for more info about middlewares and particularly
+                                        ; fun-mode.
   :middleware [m/fun-mode])
