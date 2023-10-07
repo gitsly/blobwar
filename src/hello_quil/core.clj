51dc4852e86eeefed6ed078798d50ae64d1fc24c
Martin Collberg <martin.collberg@gmail.com>
2023-10-07 23:20:14 +0200

    Make time component and separate system

--- src/hello_quil/core.clj
+++ src/hello_quil/core.clj
@@ -7,6 +7,7 @@
             [ecs.ecssystem :as ecs]
             [drawing.quildrawing :as quildrawing]
             [drawing.dbgview :as dbgview]
+            [systems.time :as systems.time]
             [clj-time [core :as t]]
             [clojure.spec.alpha :as s]
             [clojure.string :as str]
@@ -37,98 +38,83 @@
   (q/color-mode :hsb)
   (q/text-font (q/create-font "Hack" 12 true))
 
-  {:last-time (t/now)
-   :systems [(quildrawing/drawing "DrawingSys1")
-             (dbgview/->Drawing "Debug text drawing system")]
+  {:systems [(quildrawing/drawing "DrawingSys1")
+             (dbgview/->Drawing "Debug text drawing system")
+             (systems.time/->Time "Time system")]
    :circle-anim {:color 0
                  :angle 0 }})
 
 (defn update-circle
-[state]
-{:color (mod (+ (:color state) 0.7) 255)
- :angle (+ (:angle state) 0.01) })
+  [state]
+  {:color (mod (+ (:color state) 0.7) 255)
+   :angle (+ (:angle state) 0.01) })
 
 
 (defn do-systems 
-"Calls fn over a set of systems, Assuming EcsSystem realizing"
-[state
- systems
- fn]
-(loop [systems systems
-       state state]
-  (if (empty? systems) 
-    state
-    (recur (rest systems)
-           (fn (first systems) state) ; Let each 'system' update the state
-           ))))
+  "Calls fn over a set of systems, Assuming EcsSystem realizing"
+  [state
+   systems
+   fn]
+  (loop [systems systems
+         state state]
+    (if (empty? systems) 
+      state
+      (recur (rest systems)
+             (fn (first systems) state) ; Let each 'system' update the state
+             ))))
 
 
 (defn update-state [state]
-(let [now (t/now)
-      dt (t/in-millis (t/interval (:last-time state) now))]
-  (-> state
-      (assoc :dt dt)
-      (assoc :last-time now)
-      (do-systems  (:systems state) ecs/update)
-      (update-in  [:circle-anim] update-circle))))
+  (let [now (t/now)
+        dt (t/in-millis (t/interval (:last-time state) now))]
+    (-> state
+        (do-systems  (:systems state) ecs/update)
+        (update-in  [:circle-anim] update-circle))))
+
 
 (defn draw-circle
-[state]
-(q/fill (:color state) 255 255)
+  [state]
+  (q/fill (:color state) 255 255)
                                         ; Calculate x and y coordinates of the circle.
-(let [angle (:angle state)
-      x (* 150 (q/cos angle))
-      y (* 150 (q/sin angle))
-      sz (* 200 (q/sin angle))]
+  (let [angle (:angle state)
+        x (* 150 (q/cos angle))
+        y (* 150 (q/sin angle))
+        sz (* 200 (q/sin angle))]
                                         ; Move origin point to the center of the sketch.
 
-  (q/with-translation [(/ (q/width) 2)
-                       (/ (q/height) 2)]
+    (q/with-translation [(/ (q/width) 2)
+                         (/ (q/height) 2)]
                                         ; Draw the circle.
-    (q/ellipse x y 100 100))
+      (q/ellipse x y 100 100))
 
-  (q/with-translation [(+ 120 (/ (q/width) 2))
-                       (/ (q/height) 2)]
+    (q/with-translation [(+ 120 (/ (q/width) 2))
+                         (/ (q/height) 2)]
                                         ; Draw the circle.
-    (q/ellipse x y sz 200))
+      (q/ellipse x y sz 200))))
 
-
-  ))
-
-
-;;(ecs/update (dbgview/->Drawing "Debug text drawing system") )
-
-;;(update-state-via-systems 
-;; {:counter 0}
-;; [(quildrawing/drawing "DrawingSys1")
-;;  (dbgview/->Drawing "Debug text drawing system")])
-
-;;(ecs/draw 
-;; (quildrawing/->Drawing "Debug text drawing system") {})
 
 (defn draw-state [state]
-(q/background 240)
-(q/stroke-weight 2)
-;; TODO: make a system of text drawing.
+  (q/background 240)
+  (q/stroke-weight 2)
+  ;; TODO: make a system of text drawing.
                                         ;  (draw-text state)
-;;  (update-state-via-systems ) 
-(do-systems state (:systems state) ecs/draw)
+  ;;  (update-state-via-systems ) 
+  (do-systems state (:systems state) ecs/draw)
 
-(draw-circle (:circle-anim state)))
+  (draw-circle (:circle-anim state)))
 
 (q/defsketch hello-quil
-:title (str "Blob" " " "War")
-:size [640 480]
+  :title (str "Blob" " " "War")
+  :size [640 480]
                                         ; setup function called only once, during sketch initialization.
-:setup setup
+  :setup setup
                                         ; update-state is called on each iteration before draw-state.
-:update update-state
-:draw draw-state
-:features [:keep-on-top]
+  :update update-state
+  :draw draw-state
+  :features [:keep-on-top]
                                         ; This sketch uses functional-mode middleware.
                                         ; Check quil wiki for more info about middlewares and particularly
                                         ; fun-mode.
-:middleware [m/fun-mode])
-
+  :middleware [m/fun-mode])
 
 
---
+++ src/systems/time.clj
@@ -0,0 +1,28 @@
+(ns systems.time
+  (:require
+   [clj-time [core :as t]]
+   [ecs.ecssystem :as ecs]))
+
+
+(defn do-time
+  "Takes the entire component needed for the time system and updates
+  it, returns new time component"
+  [state]
+  (let [now (t/now)
+        dt (t/in-millis (t/interval (or (:last-time state) (t/now)) now))]
+    (-> state
+        (assoc :dt dt)
+        (assoc :last-time now))))
+
+(defn update-time
+  "Point out which time component in entire game-state to update with do-time"
+  [state]
+  (update-in state [:time-info] do-time))
+
+(defrecord Time[definition]
+  ecs/EcsSystem ; Realizes the EcsSystem protocol
+  (update [data state]
+    (update-time state))
+  (draw [_ state]
+    state))
+
