bd5f8d26bcfe20c854034f037d206ea2f26084a6
Martin Collberg <martin.collberg@gmail.com>
2023-10-08 13:52:15 +0200

    Feature: Let dbg drawing system not be affected by zoom and navigation-2d state

--- src/systems/dbgview.clj
+++ src/systems/dbgview.clj
@@ -1,4 +1,4 @@
-(ns drawing.dbgview
+(ns systems.dbgview
   (:require [quil.core :as q]
             [quil.middleware :as m]
             [zprint.core :as zp]
@@ -7,17 +7,20 @@
 (defn- draw-text
   [state]
   (let [text-color [128 128 128]
-        text-formatting-width 32
+        text-formatting-width 64
         text-content (zp/zprint-str state text-formatting-width)]
     ;; Note that the border (the stroke) is centered on the point where
     ;; the shape is anchored.
+    (q/push-matrix)
+    (q/reset-matrix) ; Loads the identity matrix
     (q/fill text-color)
     (q/text text-content
             10
-            10)))
+            10)
+    (q/pop-matrix)))
 
 
-(defrecord Drawing[definition]
+(defrecord Sys[definition]
   ecs/EcsSystem ; Realizes the EcsSystem protocol
   (update [data state]
     ;;    (println (:definition data))
