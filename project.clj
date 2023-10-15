f77090003c2f54c9cf774910eb80584f6085321e
Martin Collberg <martin.collberg@gmail.com>
2023-10-15 15:49:16 +0200

    Now working with only referencing another once

--- project.clj
+++ project.clj
@@ -1,10 +1,14 @@
 (defproject hello-quil "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :description "Sample game using Quil"
+  :license {:name "GPL-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [quil "3.1.0"]
                  [zprint "1.2.8"]
                  [euclidean "0.2.0"]
-                 [clj-time "0.15.2"]])
+                 [clj-time "0.15.2"]]
+
+  :main ^:skip-aot hello-quil.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
---
+++ src/hello_quil/another.clj
@@ -0,0 +1,15 @@
+(ns hello-quil.another
+  (:require
+   [clojure.string :as str]))
+
+;;(def something "apa")
+
+(println "another")
+
+(defprotocol EcsSystem
+  "A EcsSystem. Takes the entire ESC state and performs the systems
+  actions upon it. Returns the updated state"
+  (update [this state] "Update state in system, returns new state")
+  (draw [this state] "System called during draw portion of engine"))
+
+(update {:val 1} :val inc)
