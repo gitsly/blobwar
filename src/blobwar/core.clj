43e40af97578c83bd7c17d602b3dd20979fad168
Martin Collberg <martin.collberg@gmail.com>
2023-10-15 16:58:26 +0200

    Wip: add blob entity code, unsure if this is really needed but...

--- src/blobwar/core.clj
+++ src/blobwar/core.clj
@@ -9,6 +9,9 @@
    [blobwar.components.fsm]
    [blobwar.components.common]
 
+   ;; Entities (functions to apply to entities with specific set of components)
+   [blobwar.entities.blob]
+
    ;; Systems
    [blobwar.ecs.EcsSystem]
    [blobwar.systems.dbgview]
---
+++ src/blobwar/entities/blob.clj
@@ -0,0 +1,27 @@
+(ns blobwar.entities.blob
+  (:require [quil.core :as q]
+            [quil.middleware :as m]
+            [blobwar.components.common :as c]
+            [clojure.spec.alpha :as s]))
+
+
+(s/def ::blob
+  (s/and ::c/selectable
+         (s/keys :req-un [::color ::size]))) ;; Require unnamespaced keys
+
+;; Test spec
+(let [spec ::blob
+      thing {:translation [220 110]
+             :size 12
+             :selected true
+             :color [128 255 0 255]
+             :fighting {:weapon "TopLaser"
+                        :strength 12.0 }}]
+  (if (s/valid? spec thing)
+    thing
+    (s/explain-str spec thing)))
+
+(def default {:color [45 128 174 255] ; Color for the blobs should be all but red, reserve red color for blood spatter =)
+              :selected false
+              :size 10 })
+
