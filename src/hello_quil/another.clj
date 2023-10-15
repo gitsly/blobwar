3e2da92721928d765eaf3e605688069b57b74ee0
Martin Collberg <martin.collberg@gmail.com>
2023-10-15 15:51:47 +0200

    had to remove update when starting with: lein run otherwise got No single method: update of interface: hello_quil.another.EcsSystem found for function: update of protocol: EcsSystem

--- src/hello_quil/another.clj
+++ src/hello_quil/another.clj
@@ -11,5 +11,3 @@
   actions upon it. Returns the updated state"
   (update [this state] "Update state in system, returns new state")
   (draw [this state] "System called during draw portion of engine"))
-
-(update {:val 1} :val inc)
