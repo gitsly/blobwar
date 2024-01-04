c1620b611fe69208c605c9cbe8c807c7430df4c6
Martin Collberg <martin.collberg@gmail.com>
2024-01-04 13:12:30 +0100

    clarify instructions for current problem with nrepl and developing still with emacs and cider

--- README.md
+++ README.md
@@ -3,10 +3,31 @@ A minimalistic game written in clojure using quil for graphics.
 
 ## Usage
 LightTable - open `core.clj` and press `Ctrl+Shift+Enter` to evaluate the file.
+
 Emacs - run cider, open `core.clj` and press `C-c C-k` to evaluate the file.
  or `C-c M-j` to attach with CIDER to a REPL.
 
-REPL - run `(require 'blobwar.core)`.
+  Currently the jack in screws with the namespaces / state of defrecords in
+  clojure
+  and yields
+
+Exception in  :draw  function:  #error {
+ :cause No implementation of method: :update-sys of protocol: #'blobwar.ecs.EcsSystem/EcsSystem found for class: blobwar.systems.drawing.Sys
+ :via
+
+
+I suspect the current version of cider nREPL is causing issue.
+I had no problems while running:
+ refactor-nrepl 3.6.0
+ cider-nrepl 0.29.0
+
+Altough, currently the program runs nice with 'lein run'
+To develop, (workaround to get cider joy)
+One can start the repl in headless mode
+> lein repl :headless
+
+Then connect to it in emacs using: cider-connect
+Once in REPL, manually call -main method to start the app.
 
 ## License
 Copyright © 2023 Martin Collberg
--- src/blobwar/core.clj
+++ src/blobwar/core.clj
@@ -252,7 +252,8 @@ setup-parameters))
   (q/sketch  
    :size [640 480]
    :title title
-   :renderer :java2d; :opengl ; 
+   ;;:renderer :opengl ; font drawing does not work, but crisper graphics
+   :renderer :java2d ; a little blurrier 
                                         ; setup function called only once, during sketch initialization.
    :setup (fn [] (setup setup-parameters))
 
@@ -288,21 +289,4 @@ setup-parameters))
   (println "In the absence of parantheses, chaos prevails")
   (create-sketch "Blob war" {:owner :player-1 }))
 
-;; Uncomment below and execute to open a new sketch in the CIDER REPL
-
-;; Haze command
-;;  Startup: /usr/bin/lein update-in :dependencies conj \[nrepl/nrepl\ \"1.0.0\"\] -- update-in :dependencies conj \[refactor-nrepl/refactor-nrepl\ \"3.9.0\"\] -- update-in :plugins conj \[refactor-nrepl/refactor-nrepl\ \"3.9.0\"\] -- update-in :plugins conj \[cider/cider-nrepl\ \"0.40.0\"\] -- repl :headless :host localhost
-
-;; OF-WL4775 (works better), Java 1.8
-;; refactor-nrepl 3.6.0
-;; cider-nrepl 0.29.0
-
-;; Was able to reproduce the exact same issue with the later cider versions
-;; on the windows machine as well
-
-(println "Create sketch")
-(qa/current-applet)
-
-(def main-applet (atom (create-sketch "Blob war" {:owner :player-1 })))
-
-(println @main-applet)
+;; (-main) ; Uncomment and run when having used :headless REPL and cider-connect
